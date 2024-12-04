eedd50b6617ede8f1a69a49adfc1d918b2837f4a
furqonstudio <furqonstudio@gmail.com>
2024-12-04 11:51:52 +0700

    fix: update ProjectCard

--- src/components/molecules/ProjectCard.tsx
+++ src/components/molecules/ProjectCard.tsx
@@ -2,51 +2,48 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 interface ProjectCardProps {
-  image: string
+  imageSrc: string
   title: string
   description: string
-  textDark?: boolean
+  textLight?: boolean
   bgColor?: string
+  imageClassName: string
 }
 
 export const ProjectCard: React.FC<ProjectCardProps> = ({
-  image,
+  imageSrc,
   title,
   description,
   bgColor = 'bg-yellow-400',
-  textDark = false,
+  textLight = false,
+  imageClassName,
 }) => {
   return (
-    <Link href={'/projects'}>
-      <div
-        className={`${bgColor}  p-4 rounded-2xl space-y-2 hover:scale-95 transition ease-in-out duration-700 lg:w-full`}
+    <Link
+      href={'/projects'}
+      className={`${bgColor} md:row-span-2 h-full rounded-2xl aspect-square md:aspect-auto py-4 px-8 relative overflow-hidden`}
+    >
+      <h3
+        className={`font-gasoek text-xl text-center ${
+          textLight && 'text-white'
+        }`}
       >
-        <div>
-          <h3
-            className={`2xl:text-3xl font-inter font-bold tracking-tight text-lg -mb-1 ${
-              textDark ? 'text-gray-900' : 'text-white'
-            }`}
-          >
-            {title}
-          </h3>
-          <p
-            className={`2xl:text-xl font-inter text-sm ${
-              textDark ? 'text-gray-800' : 'text-gray-200'
-            }`}
-          >
-            {description}
-          </p>
-        </div>
-        <div className="relative w-full aspect-[16/9]">
-          <Image
-            src={image}
-            alt="project-thumbnail"
-            layout="fill"
-            objectFit="cover"
-            className="rounded-xl"
-          />
-        </div>
-      </div>
+        {title}
+      </h3>
+      <p
+        className={`text-sm font-inter text-center   ${
+          textLight && 'text-white'
+        }`}
+      >
+        {description}
+      </p>
+      <Image
+        alt="project1"
+        src={imageSrc}
+        width={500}
+        height={500}
+        className={`absolute rounded-xl ${imageClassName}`}
+      />
     </Link>
   )
 }
--- src/components/section/ProjectsSection.tsx
+++ src/components/section/ProjectsSection.tsx
@@ -1,47 +1,91 @@
+import Image from 'next/image'
 import { SectionTitle } from '../atomic/SectionTitle'
 import { ProjectCard } from '../molecules/ProjectCard'
 
 export const ProjectSection = () => {
-  const Projects = [
-    {
-      id: 1,
-      imageUrl: '/img.jpg',
-      title: 'Prelovnes',
-      description: 'UNNES preloved marketplace mobile',
-      bgColor: 'bg-yellow-400',
-      isTextDark: true,
-    },
-    {
-      id: 2,
-      imageUrl: '/img.jpg',
-      title: 'Prelovnes',
-      description: 'UNNES preloved marketplace mobile',
-      bgColor: 'bg-yellow-400',
-      isTextDark: true,
-    },
-    {
-      id: 3,
-      imageUrl: '/img.jpg',
-      title: 'Prelovnes',
-      description: 'UNNES preloved marketplace mobile',
-      bgColor: 'bg-lime-400',
-      isTextDark: true,
-    },
-  ]
+  // const Projects = [
+  //   {
+  //     id: 1,
+  //     imageUrl: '/img.jpg',
+  //     title: 'Prelovnes',
+  //     description: 'UNNES preloved marketplace mobile',
+  //     bgColor: 'bg-yellow-400',
+  //     isTextDark: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     imageUrl: '/img.jpg',
+  //     title: 'Prelovnes',
+  //     description: 'UNNES preloved marketplace mobile',
+  //     bgColor: 'bg-yellow-400',
+  //     isTextDark: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     imageUrl: '/img.jpg',
+  //     title: 'Prelovnes',
+  //     description: 'UNNES preloved marketplace mobile',
+  //     bgColor: 'bg-lime-400',
+  //     isTextDark: true,
+  //   },
+  // ]
   return (
     <section className="section-style">
-      <SectionTitle text="PROJECTS" />
-      <div className="2xl:gap-6 gap-4 grid md:grid-cols-2">
-        {Projects.map((project) => (
-          <ProjectCard
-            key={project.id}
-            image={project.imageUrl}
-            title={project.title}
-            description={project.description}
-            textDark={project.isTextDark}
-            bgColor={project.bgColor}
+      <h2 className="text-white font-gasoek text-center text-3xl 2xl:text-5xl">
+        PROJECTS
+      </h2>
+      <div className="grid md:grid-cols-4 md:grid-rows-3 md:gap-4 gap-4">
+        <div className="bg-yellow-400 md:row-span-2 h-full rounded-2xl aspect-square md:aspect-auto py-4 px-8 relative overflow-hidden">
+          <h3 className="font-gasoek text-xl text-center">Prelovnes</h3>
+          <p className="text-sm font-inter text-center">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <Image
+            alt="project1"
+            src={'/assets/projects/prelovnes.jpeg'}
+            width={500}
+            height={500}
+            className="absolute left-1/2 bottom-0 translate-x-[-50%] translate-y-[50%] w-52 rounded-xl"
           />
-        ))}
+        </div>
+
+        <ProjectCard
+          title="Xballacc"
+          description="Website toko online aksesoris HP"
+          imageSrc="/assets/projects/xballacc.jpg"
+          bgColor="bg-green-500"
+          imageClassName="left-1/2 w-72 translate-x-[-50%] translate-y-[50%]"
+        />
+        {/* <div className="bg-green-600  rounded-xl h-full aspect-square" /> */}
+
+        <div className="bg-blue-600 md:col-span-2 rounded-xl h-full aspect-square" />
+
+        <div className="bg-red-600 h-44 md:col-span-2 rounded-xl h-full aspect-square" />
+        <div className="bg-neutral-800 text-white h-full md:row-span-2 p-4 overflow-hidden relative rounded-2xl">
+          <h3 className="font-gasoek text-lg">Macode.</h3>
+          <p className="text-sm max-w-28">Slicing UI Mobile</p>
+          <Image
+            alt="project1"
+            src={'/assets/projects/macode.jpg'}
+            width={500}
+            height={500}
+            className="absolute w-48 rounded-lg mt-4"
+          />
+        </div>
+        <div className="md:col-span-2 rounded-2xl p-4 relative overflow-hidden bg-neutral-800 text-white">
+          <h3 className="font-gasoek text-lg">SIPEKAT</h3>
+          <p className="text-sm max-w-28">
+            Sistem Informasi Perawatan Kendaraan Terpusat
+          </p>
+          <Image
+            alt="project1"
+            src={'/assets/projects/sipekat.jpg'}
+            width={500}
+            height={500}
+            className="absolute -bottom-2 -right-4 w-80 rounded-lg"
+          />
+        </div>
+        <div className="bg-yellow-600  rounded-xl" />
       </div>
     </section>
   )
