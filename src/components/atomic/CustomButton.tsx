9d780b45986ccec1ca5be790687e27c60d096257
furqonstudio <furqonstudio@gmail.com>
2025-01-10 13:30:43 +0700

    feat: add animation in ProfileSection

--- src/components/atomic/CustomButton.tsx
+++ src/components/atomic/CustomButton.tsx
@@ -1,11 +1,20 @@
+import { motion } from 'motion/react'
+
 interface CustomButtonProps {
   text: string
 }
 
 export const CustomButton: React.FC<CustomButtonProps> = ({ text }) => {
   return (
-    <div className="bg-red-600 p-4 text-lg text-center rounded-2xl font-gasoek text-white hover:cursor-pointer hover:scale-95 transition ease-in-out duration-700 md:max-w-md md:m-auto">
+    <motion.div
+      className="bg-red-600 p-4 text-lg text-center rounded-2xl font-gasoek text-white hover:cursor-pointer md:max-w-md md:m-auto"
+      initial={{ opacity: 0, y: 20 }} // Keadaan awal: tidak terlihat dan sedikit turun
+      animate={{ opacity: 1, y: 0 }} // Keadaan akhir: terlihat dan di posisi normal
+      transition={{ duration: 0.5 }} // Durasi transisi
+      whileHover={{ scale: 1.05 }} // Efek saat hover
+      whileTap={{ scale: 0.95 }} // Efek saat ditekan
+    >
       {text}
-    </div>
+    </motion.div>
   )
 }
--- src/components/section/ProfileSection.tsx
+++ src/components/section/ProfileSection.tsx
@@ -1,32 +1,50 @@
 import Image from 'next/image'
 import { CustomButton } from '../atomic/CustomButton'
 import { SocialMedia } from '../molecules/SocialMedia'
+import { motion } from 'motion/react'
 
 export const ProfileSection = ({ id }: { id: string }) => {
   return (
     <section id={id} className="lg:mr-0 section-style">
-      <div className="text-white text-center">
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="text-white text-center"
+      >
         <h1 className="font-gasoek text-4xl">
           <span className="inline">FURQON</span>
           <span className="block sm:inline lg:hidden"> AL-RASYID</span>
           <span className="hidden lg:block"> AL-RASYID</span>
         </h1>
         <p className="font-inter">Software Engineer</p>
-      </div>
+      </motion.div>
       <div className="space-y-4">
-        <Image
-          alt="furqon-al-rasyid"
-          src={'/assets/furqon-avatar.png'}
-          width={400}
-          height={400}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6 }}
           className="m-auto"
-        />
+        >
+          <Image
+            alt="furqon-al-rasyid"
+            src={'/assets/furqon-avatar.png'}
+            width={400}
+            height={400}
+            className="m-auto"
+          />
+        </motion.div>
         <CustomButton text="CONTACT ME" />
       </div>
-      <div className="space-y-2 text-center">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="space-y-2 text-center"
+      >
         <p className="font-inter text-white">My Socials</p>
         <SocialMedia />
-      </div>
+      </motion.div>
     </section>
   )
 }
