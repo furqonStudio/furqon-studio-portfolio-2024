68d87da84857a539d8fe3aefd0bf7a8c6a60acbb
furqonstudio <furqonstudio@gmail.com>
2025-01-10 19:36:42 +0700

    fix: add useInView in Button

--- src/components/atomic/CustomButton.tsx
+++ src/components/atomic/CustomButton.tsx
@@ -1,18 +1,25 @@
 import { motion } from 'motion/react'
+import { useInView } from 'react-intersection-observer'
 
 interface CustomButtonProps {
   text: string
 }
 
 export const CustomButton: React.FC<CustomButtonProps> = ({ text }) => {
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  })
+
   return (
     <motion.div
+      ref={ref} // Attach the ref here
       className="bg-red-600 p-4 text-lg text-center rounded-2xl font-gasoek text-white hover:cursor-pointer md:max-w-md md:m-auto"
-      initial={{ opacity: 0, y: 20 }} // Keadaan awal: tidak terlihat dan sedikit turun
-      animate={{ opacity: 1, y: 0 }} // Keadaan akhir: terlihat dan di posisi normal
-      transition={{ duration: 0.5 }} // Durasi transisi
-      whileHover={{ scale: 1.05 }} // Efek saat hover
-      whileTap={{ scale: 0.95 }} // Efek saat ditekan
+      initial={{ opacity: 0, scale: 0.8 }} // Initial state: invisible
+      animate={inView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.8 }} // Animate based on inView
+      transition={{ duration: 0.6 }} // Transition duration
+      whileHover={{ scale: 1.1 }} // Scale up to 110% on hover
+      whileTap={{ scale: 0.95 }} // Scale down to 95% when pressed
     >
       {text}
     </motion.div>
--- src/components/section/ContactSection.tsx
+++ src/components/section/ContactSection.tsx
@@ -1,14 +1,32 @@
 import { CustomButton } from '../atomic/CustomButton'
 import { SocialMedia } from '../molecules/SocialMedia'
+import { useInView } from 'react-intersection-observer'
+import { motion } from 'motion/react'
 
 export const ContactSection = ({ id }: { id: string }) => {
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  })
+
   return (
-    <section id={id} className="section-style lg:hidden">
-      <h2 className="text-white font-gasoek text-center text-3xl">
+    <section id={id} ref={ref} className="section-style lg:hidden">
+      <motion.h2
+        className="text-white font-gasoek text-center text-3xl"
+        initial={{ opacity: 0, y: -20 }}
+        animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }}
+        transition={{ duration: 0.6 }}
+      >
         Let&apos;s Work Together!
-      </h2>
+      </motion.h2>
       <CustomButton text="CONTACT ME" />
-      <SocialMedia />
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
+      >
+        <SocialMedia />
+      </motion.div>
     </section>
   )
 }
