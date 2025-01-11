6326e14db333c1f7291bcd8ffc65f54a8b23da0c
furqonstudio <furqonstudio@gmail.com>
2025-01-11 19:46:05 +0700

    fix: add scale animation in button

--- src/components/atomic/CustomButton.tsx
+++ src/components/atomic/CustomButton.tsx
@@ -13,8 +13,8 @@ export const CustomButton: React.FC<CustomButtonProps> = ({ text }) => {
     <motion.div
       ref={ref}
       className={`bg-red-600 hover:bg-purple-600 transition-all duration-300 ease-in-out hover:scale-95 p-4 text-lg text-center rounded-2xl font-gasoek text-white hover:cursor-pointer md:max-w-md md:m-auto `}
-      initial={{ opacity: 0 }}
-      animate={isInView ? { opacity: 1 } : { opacity: 0 }}
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={isInView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.9 }}
       transition={{ duration: 0.5 }}
     >
       {text}
